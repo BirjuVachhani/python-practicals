8ce6573b8179bf9212ad702c06a83ce45414dcb2
DESKTOP-8ED1V8B\Believe <birjuvachhani96@gmail.com>
2018-03-27 12:30:06 +0530

    complete 5th of Set 5

---
+++ Set 5/PR5.py
@@ -0,0 +1,23 @@
+from os import listdir
+from os.path import isfile, abspath, join
+
+def get_files(file_dict,dir_name):
+    dir_data = listdir(dir_name)
+    for dir in dir_data:
+        path = join(dir_name,dir)
+        if isfile(path):
+            file_dict[dir] = abspath(path)
+        else:
+            get_files(file_dict,join(dir_name,dir))
+
+def create_path(path):
+    path_parts = path.split('\\')
+    return '\\\\'.join(path_parts)
+
+loc = input('Enter path: ')
+path = create_path(loc)
+print(path)
+file_dict = dict()
+get_files(file_dict,loc)
+for key,value in file_dict.items():
+    print('{}:\n{}\n\n'.format(key,value))
