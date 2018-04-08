43d4fcd6dea69269d684de244f964574be2d2e82
DESKTOP-8ED1V8B\Believe <birjuvachhani96@gmail.com>
2018-04-08 17:12:00 +0530

    Complete 1st of Set 7

--- README.md
+++ README.md
@@ -92,7 +92,7 @@ For example:
  - [ ] 3. WAP to demonstrate usage of Python debugging module.
 
  ## Set 7
- - [ ] 1. Rewrite Spam confidence program (5.2) using try and except.
+ - [x] 1. Rewrite Spam confidence program (5.2) using try and except.
  - [ ] 2. WAP to read through the mail box data and when you find line that starts with “From”, you will split the line into words using the split function. We are
 interested in who sent the message, which is the
 second word on the From line.
---
+++ Set 7/PR1.py
@@ -0,0 +1,17 @@
+pattern = 'X-DSPAM-Confidence'
+file_name = input('Enter the file name to read from(with extension):')
+confidences = list()
+try:
+    with open(file_name) as f:
+        for line in f:
+            line_parts = line.strip().split(':')
+            if line_parts[0] == pattern:
+                confidences.append(float('line_parts[1]'))
+    print('Average spam confidence: {}'.format(sum(confidences)/len(confidences)))
+except IOError as e:
+    print(e)
+except ValueError as v:
+    print('Unable to convert confidence value to float: {}'.format(v))
+except ZeroDivisionError:
+    print('No confidence values found in the file.')
+
